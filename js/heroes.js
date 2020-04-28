957b42ff1c0dd1763534fbab85508d20e882606f
mamatom <mamatom@users.noreply.github.com>
2020-04-28 23:18:47 +0700

    update json

--- js/heroes.js
+++ js/heroes.js
@@ -1,48 +1,127 @@
-
 var lastResultsId = [];
 $.holdReady(true);
-var heroesJson = $.getJSON("./json/newHeroes.json", function (json) {
-    //console.log(json);
-    heroesJson = json;
+var heroesJson = $.getJSON("./json/Heroes.json", function (heroes_json) {
+    console.log(heroes_json);
+    heroesJson = heroes_json;
+    $.holdReady(false);
+    
+
+});
+$.holdReady(true);
+var dataJson = $.getJSON("./json/data.json", function (data_json) {
+    console.log(data_json);
+    dataJson = data_json;
+    
     $.holdReady(false);
 });
 
+var allData = {}; 
 
- var newHeroJson = {};
+
+
+
 
 function newJson(){
     $.each(heroesJson, function (_,hero){
-        var id = hero.id
-        //newHeroJson['id'+('00' + (id)).slice(-3)] = hero;
-        newHeroJson['heroId'+id] = hero;
+        var id = hero.id;
+        if(!dataJson['heroId'+id]){
+            dataJson['heroId'+id]= {};
+        }
+        var data = dataJson['heroId'+id];
 
+
+        if(!data.rarity)
+            data.rarity = "";
+        if(!data.evo)
+            data.evo = "";
+        if(!data.d)
+            data.d = [];
+        if(!data.hold_d)
+            data.hold_d =[];
+        if(!data.half_d)
+            data.half_d =[];
+        if(!data.dash)
+            data.dash = [];
+        if(!data.dash_half_d)
+            data.dash_half_d =[];
+        if(!data.dash_hold_d)
+            data.dash_hold_d = [];
+        if(!data.air_d)
+            data.air_d = [];
+        if(!data.air_half_d)
+            data.air_half_d = [];
+        if(!data.air_hold_d)
+            data.air_hold_d = [];
+        if(!data.air_dash)
+            data.air_dash = [];
+        if(!data.special)
+            data.special = [];
+        if(!data.counter)
+            data.counter = [];
+        if(!data.sd)
+            data.sd = [];
+        if(!data.tester)
+            data.tester = "";
+        if(!data.note)
+            data.note = "";
+
+
+
+
+            newhero = {
+            "name":hero.name,
+            "id":hero.id,
+            "type":hero.type,
+            "rarity": data.rarity,
+            "evo": data.evo,
+            "d": data.d,
+            "hold_d": data.hold_d,
+            "half_d":data.half_d,
+            "dash":data.dash,
+            "dash_half_d":data.dash_half_d,
+            "dash_hold_d":data.dash_hold_d,
+            "air_d":data.air_d,
+            "air_half_d":data.air_half_d,
+            "air_hold_d":data.air_hold_d,
+            "air_dash":data.air_dash,
+            "special":data.special,
+            "counter":data.counter,
+            "sd":data.sd,
+            "tester":data.tester,
+            "note":data.note
+
+            }
+
+        allData['heroId'+id] = newhero;
 
     });
-    console.log(JSON.stringify(newHeroJson));
+    //console.log(allData);
+    console.log(JSON.stringify(allData));
 
 }
 
-function selectHero(heroId){
+function selectHero(heroId) {
     $('#resultsContainer').hide();
-    var id= ('00' + (heroId.id)).slice(-3);
-    $('#heroImg').attr('src','img/heroes sprite/'+id+'_M.png')
+    var id = ('00' + (heroId.id)).slice(-3);
+    $('#heroImg').attr('style', 'background-image: url("img/heroes sprite/'+ id +'_M.png");');
     console.log(id);
 }
 
 
 
 $(document).ready(function populateHeroes() {
-    
+
     var image;
     $.each(heroesJson, function (_, hero) {
         var imgPos = -1 * ((hero.id - 1) * 48);
         var image = '<a class="resultsImg" style="background: url(./img/heroFace.jpg) ' + imgPos + 'px 0px;"></a>';
-        var list = '<div class="result" onclick="selectHero(heroesJson.heroId' + hero.id+')" id="heroId' + hero.id + '">' + image + '<div class="resultsTxt">' + hero.name + '</div></div>'
+        var list = '<div class="result" onclick="selectHero(heroesJson.heroId' + hero.id + ')" id="heroId' + hero.id + '">' + image + '<div class="resultsTxt">' + hero.name + '</div></div>'
         $("#resultsContainer").append(list);
         lastResultsId.push(hero.id);
     })
     $("#resultsContainer").hide();
-});1
+});
+1
 
 
 function filterHeroes(name, isenter = false) {
